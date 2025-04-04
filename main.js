c2e1eea292d0a234f442fb404c76e0c3fe38436e
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-04-04 21:24:18 +0100

    update

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ addBook('Hunger Jay', 'Jay', 'action', 780, true, '1997-07-31')
 addBook('Attack on Jay', 'Eren Jayger', 'thriller', 800, false,'0001-01-01')
 addBook('Jay of the Rings', 'Jay', 'Fantasy', 400, false, '2023-04-06')
 addBook('Jaydarella', 'Jay', 'Fantasy', 440, false, '2023-07-06')
+addBook('Jayrassic World', 'Jay', 'Biography', 5050, false, '2000-06-09')
 const main = document.querySelector('.main')
 const displayBooks = function(library) {
     main.innerHTML = ''
